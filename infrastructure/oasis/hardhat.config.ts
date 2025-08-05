bf958271dd10b8d59da78b97f4f5c85be79e3fec
Marcussy34 <marcus.tanchiyau@gmail.com>
2025-08-05 18:28:21 +0800

    added index.ts and imported tasks in hardhat config

--- infrastructure/oasis/hardhat.config.ts
+++ infrastructure/oasis/hardhat.config.ts
@@ -1,6 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@oasisprotocol/sapphire-hardhat";
 import "@nomicfoundation/hardhat-toolbox";
+import "./tasks";
 import * as dotenv from "dotenv";
 
 // Load environment variables from .env file
